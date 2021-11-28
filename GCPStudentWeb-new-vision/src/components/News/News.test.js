ddda5832d80af1b71a7a4d04d6f032ef939bb87d
KENNISKWAN <kwskennis@gmail.com>
2021-11-28 12:03:09 +0800

    Updates

--- GCPStudentWeb-new-vision/src/components/News/News.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import News from './News';
-
-test('renders learn react link', () => {
-    render(<News />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});
